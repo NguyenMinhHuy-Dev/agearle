f9f5302cb335982f8d07c835c4cb04ac76a334e2
FingPong <114319368+FingPong@users.noreply.github.com>
2023-04-24 16:13:36 +0700

    add useGetData

---
+++ src/custom-hooks/useGetData.js
@@ -0,0 +1,32 @@
+// import React from 'react'
+import { db } from '../config/firebase'
+import { collection, onSnapshot } from 'firebase/firestore'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+const useGetData = (collectionName) => {
+
+    const [data, setData] = useState([])
+    const [loading,setLoading] = useState([])
+    const collectionRef = collection(db,collectionName)
+
+    useEffect(() => {
+        const getData = async() => {
+
+            //===firebase firestore realtime data update====
+
+            await onSnapshot(collectionRef,(snapshot)=>{
+                setData(snapshot.docs.map(doc=>({...doc.data(), id: doc.id})))
+                setLoading(false);
+            });
+            
+        }
+
+        getData()
+    },[collectionRef])
+
+  return { data,loading }; 
+    
+}
+
+export default useGetData
